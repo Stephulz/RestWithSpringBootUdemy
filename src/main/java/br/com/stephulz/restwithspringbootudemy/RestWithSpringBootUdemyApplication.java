433a6112187501fcf8cf482491883b92c26740aa
Stephulz <stefansanches9@gmail.com>
2020-02-02 01:14:33 -0300

    Hello Spring Boot and REST Udemy

---
+++ src/main/java/br/com/stephulz/restwithspringbootudemy/RestWithSpringBootUdemyApplication.java
@@ -0,0 +1,13 @@
+package br.com.stephulz.restwithspringbootudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringBootUdemyApplication.class, args);
+	}
+
+}
